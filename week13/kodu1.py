cdb5f6c9ce29c92ffc635eca66a7a16d8ce51ae6
Priit Ohlo <priit.ohlo@rmit.ee>
2019-11-28 12:48:17 +0200

    fix wk13

--- week13/kodu1.py
+++ week13/kodu1.py
@@ -1,5 +1,7 @@
 def auto_hind(price, years):
     if years == 0:
-        return round(price, 2)
+        return price
     else:
-        return auto_hind(price * 0.8, years - 1)
+        return auto_hind(round(price * 0.8, 2), years - 1)
+
+print(auto_hind(6788.46, 2))
